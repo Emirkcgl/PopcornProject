62b96544af1b84dca95dab423a00b32cfaefa8cb
Emirkcgl <yunus0598koca@gmail.com>
2025-09-01 15:04:30 +0300

    create-component startRating

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import StarRating from "./starRating";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*  <App /> */}
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
   </React.StrictMode>
 );
