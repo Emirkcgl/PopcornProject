b5640ccfa5353d03bcaf830dba99067e56eb8c3f
Emirkcgl <yunus0598koca@gmail.com>
2025-09-08 12:12:26 +0300

    Add hover effect and onSetRating prop to StarRating component.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/*  <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      onSetRating={(newRating) =>
+        console.log("Yeni derecelendirme:", newRating)
+      }
+    />
   </React.StrictMode>
 );
